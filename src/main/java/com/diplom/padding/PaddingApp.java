07b4d724e3054d2a09f92a3c05ee68e269810b33
SavelevDenis <denis.savelev.1999@gmail.com>
2021-04-22 20:55:45 +0300

    Init project

---
+++ src/main/java/com/diplom/padding/PaddingApp.java
@@ -0,0 +1,11 @@
+package com.diplom.padding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaddingApp {
+    public static void main(String[] args) {
+        SpringApplication.run(PaddingApp.class, args);
+    }
+}

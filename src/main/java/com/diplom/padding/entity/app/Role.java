b65be426a7e907663153d0ca006378f1f2b532e6
SavelevDenis <denis.savelev.1999@gmail.com>
2021-05-22 21:00:15 +0300

    add role

---
+++ src/main/java/com/diplom/padding/entity/app/Role.java
@@ -0,0 +1,34 @@
+package com.diplom.padding.entity.app;
+
+import javax.persistence.*;
+
+@Entity
+public class Role {
+    @Id
+    private Integer id;
+    private String name;
+
+    public Role() {
+    }
+
+    public Role(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

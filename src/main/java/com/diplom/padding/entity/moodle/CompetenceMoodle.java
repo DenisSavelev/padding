51c847e6f869fc5e11a978d6d45f928e1a0f5701
SavelevDenis <denis.savelev.1999@gmail.com>
2021-05-30 18:28:28 +0300

    refactor entity

--- src/main/java/com/diplom/padding/entity/moodle/CompetenceMoodle.java
+++ src/main/java/com/diplom/padding/entity/moodle/CompetenceMoodle.java
@@ -11,37 +11,22 @@ public class CompetenceMoodle {
     private String shortname;
     @Column(name = "description")
     private String description;
-
-    public CompetenceMoodle() {
-    }
-
-    public CompetenceMoodle(Long id, String shortname, String description) {
-        this.id = id;
-        this.shortname = shortname;
-        this.description = description;
-    }
+    @Column(name = "timemodified")
+    private Long modified;
 
     public Long getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getShortname() {
         return shortname;
     }
 
-    public void setShortname(String shortname) {
-        this.shortname = shortname;
-    }
-
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public Long getModified() {
+        return modified;
     }
 }
--- src/main/java/com/diplom/padding/entity/moodle/CompetenceMoodle2.java
+++ src/main/java/com/diplom/padding/entity/moodle/CompetenceMoodle2.java
@@ -15,56 +15,34 @@ public class CompetenceMoodle2 {
     private Long idCompetence;
     @Column(name = "parentid")
     private Long idParent;
+    @Column(name = "timemodified")
+    private Long modified;
 
     public CompetenceMoodle2() {
 
-    }
-
-    public CompetenceMoodle2(Long id, String shortname, String description, Long idCompetence, Long idParent) {
-        this.id = id;
-        this.shortname = shortname;
-        this.description = description;
-        this.idCompetence = idCompetence;
-        this.idParent = idParent;
     }
 
     public Long getId() {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getShortname() {
         return shortname;
-    }
-
-    public void setShortname(String shortname) {
-        this.shortname = shortname;
     }
 
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
     public Long getIdCompetence() {
         return idCompetence;
-    }
-
-    public void setIdCompetence(Long idCompetence) {
-        this.idCompetence = idCompetence;
     }
 
     public Long getIdParent() {
         return idParent;
     }
 
-    public void setIdParent(Long idParent) {
-        this.idParent = idParent;
+    public Long getModified() {
+        return modified;
     }
 }

a35023a46b9961c7c9890f044b32fda671a92332
SavelevDenis <denis.savelev.1999@gmail.com>
2021-06-03 21:12:15 +0300

    add get data for the day

--- src/main/java/com/diplom/padding/dao/impl/CourseTaskCompetenceMoodleDAOImpl.java
+++ src/main/java/com/diplom/padding/dao/impl/CourseTaskCompetenceMoodleDAOImpl.java
@@ -7,6 +7,7 @@ import com.diplom.padding.entity.moodle.CourseTaskCompetenceMoodle;
 
 import javax.persistence.*;
 import javax.persistence.criteria.*;
+import java.util.Date;
 import java.util.List;
 
 @Repository
@@ -24,6 +25,16 @@ public class CourseTaskCompetenceMoodleDAOImpl implements CourseTaskCompetenceMo
         CriteriaQuery<Long> cq = cb.createQuery(Long.class);
         Root<CourseTaskCompetenceMoodle> root = cq.from(CourseTaskCompetenceMoodle.class);
         cq.select(root.get("idCompetence")).where(cb.equal(root.get("idCourseTask"), id));
+        return entityManager.createQuery(cq).getResultList();
+    }
+
+    @Override
+    public List<CourseTaskCompetenceMoodle> findForTheDay() {
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<CourseTaskCompetenceMoodle> cq = cb.createQuery(CourseTaskCompetenceMoodle.class);
+        Root<CourseTaskCompetenceMoodle> root = cq.from(CourseTaskCompetenceMoodle.class);
+        Predicate time = cb.lessThan(cb.diff((new Date().getTime() / 1000), root.get("modified")), 88200L);
+        cq.where(time);
         return entityManager.createQuery(cq).getResultList();
     }
 }
